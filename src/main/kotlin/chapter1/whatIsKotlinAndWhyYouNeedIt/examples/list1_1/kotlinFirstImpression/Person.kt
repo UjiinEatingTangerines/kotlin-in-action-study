85ccf37351a84c399add56a17af117c70b7ac1da
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-04-27 10:59:15 +0900

    Modifying Kotlin Preferences and Examples

--- src/main/kotlin/chapter1/whatIsKotlinAndWhyYouNeedIt/examples/list1_1/kotlinFirstImpression/Person.kt
+++ src/main/kotlin/chapter1/whatIsKotlinAndWhyYouNeedIt/examples/list1_1/kotlinFirstImpression/Person.kt
@@ -1,6 +1,21 @@
 package org.example.chapter1.whatIsKotlinAndWhyYouNeedIt.examples.list1_1.kotlinFirstImpression
 
-// data class
+/*
+1. 슈퍼 클래스: 어떤 클래스가 상속을 받을 때 기준이 되는 부모 클래스
+슈퍼 클래스 = 다른 클래스가 상속하는 부모 클래스
+코틀린에선 open 키워드를 붙여야 상속이 가능하다
+
+ */
+
+/*
+[data class]
+다양한 메소드를 자동으로 생성해주는 클래스
+ex) hashCode(), copy(), equals(), toString(), componentsN() ... etc
+
+1. 기본 생성자에 1개 이상의 파라미터가 있어야 함
+2. 기본 생성자의 파라미터가 val 또는 var 로 선언되어야 함
+3. 다른 클래스를 상속받을 수 없음(*1.슈퍼 클래스를 가질 수 없음)
+ */
 data class Person (
     // 읽기 전용 프로퍼티
     val name: String,
